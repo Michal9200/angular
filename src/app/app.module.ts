00c3dd28bc50917ed531cbf20aecc17a679ea934
Michal <michalbu92@o2.pl>
2020-10-03 14:33:40 +0200

    zadanie 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyDirectiveDirective } from './my-directive.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    MyDirectiveDirective,
 
   ],
   imports: [
