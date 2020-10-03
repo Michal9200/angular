980f3bfa1d077103c9aa94a0dd9620108c92ebcf
Michal <michalbu92@o2.pl>
2020-10-03 13:36:51 +0200

    zadanie 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  topMovies = [
-    {title: 'Skazani na Shawshank', director: 'Frank Darabont'},
-    {title: 'Ojciec chrzestny', director: 'Francis Ford Coppola'},
-    {title: 'Ojciec chrzestny II', director: 'Francis Ford Coppola'},
-    {title: 'Mroczny rycerz', director: 'Christopher Nolan'},
-    {title: 'Dwunastu gniewnych ludzi', director: 'Sidney Lumet'},
-  ];
+  tab = 0;
+  setTab(num: number): void {
+    this.tab = num;
+  }
+  isSelected(num: number): boolean{
+    return this.tab === num;
+  }
 }
---
+++ src/app/tab-content-component/tab-content-component.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tab-content-component',
+  templateUrl: './tab-content-component.component.html',
+  styleUrls: ['./tab-content-component.component.css']
+})
+export class TabContentComponentComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
