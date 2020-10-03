67822669a9576cd50e07078b4d453a8251f16b38
Michal <michalbu92@o2.pl>
2020-10-03 16:06:51 +0200

    zadanie 9

--- src/app/my-directive.directive.ts
+++ src/app/my-directive.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appMyDirective]'
@@ -7,6 +7,17 @@ export class MyDirectiveDirective {
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {
     renderer.setStyle(elementRef.nativeElement, 'backgroundColor', 'red');
+  }
+  @HostListener('mouseenter')
+  onMouseenter(): void{
+    this.setBgColor('yellow');
+  }
+  @HostListener('mouseleave')
+  onMouseLeave(): void{
+    this.setBgColor('white');
+  }
+  private setBgColor(color: string): void{
+    this.renderer.setStyle(this.elementRef.nativeElement, 'backgroundColor', color );
   }
 
 }
