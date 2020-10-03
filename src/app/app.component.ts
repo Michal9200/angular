54db78b981cdf5f37efceeeca89ec5d888b67fd0
Michal <michalbu92@o2.pl>
2020-10-03 13:59:12 +0200

    zadanie 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tab = 0;
-  setTab(num: number): void {
-    this.tab = num;
+export class AppComponent implements OnInit{
+  borderStyle = '1px solid black';
+alertStyle = {
+  color : 'red',
+  fontWeight : 'bold',
+  borderBottom: this.borderStyle,
+};
+style: any;
+
+  // tslint:disable-next-line:typedef
+ setStyle(style: any){
+  this.style = style;
+}
+
+  ngOnInit(): void {
+   this.setStyle(this.alertStyle);
   }
-  isSelected(num: number): boolean{
-    return this.tab === num;
-  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { MyComponentComponent } from './my-component/my-component.component';
-import { TabComponentComponent } from './tab-component/tab-component.component';
-import { TabContentComponentComponent } from './tab-content-component/tab-content-component.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent,
-    TabComponentComponent,
-    TabContentComponentComponent
+
   ],
   imports: [
     BrowserModule
