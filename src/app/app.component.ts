99a39e0b1aa9a4ee9fd802ee36834ce6952ce380
Michal <michalbu92@o2.pl>
2020-10-03 11:54:58 +0200

    zadanie 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  exists = false;
-  toggleExists(): void{
-    this.exists = !this.exists;
-  }
+  topMovies = [
+    {title: 'Skazani na Shawshank', director: 'Frank Darabont'},
+    {title: 'Ojciec chrzestny', director: 'Francis Ford Coppola'},
+    {title: 'Ojciec chrzestny II', director: 'Francis Ford Coppola'},
+    {title: 'Mroczny rycerz', director: 'Christopher Nolan'},
+    {title: 'Dwunastu gniewnych ludzi', director: 'Sidney Lumet'},
+  ];
 }
