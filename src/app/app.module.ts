eb3e968ef0bc41f177024b5d7ce01a3147914766
Michal <michalbu92@o2.pl>
2020-10-04 13:33:56 +0200

    zadanie 20

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,42 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MyDirectiveDirective } from './my-directive.directive';
-import { MyPipePipe } from './my-pipe.pipe';
-import { ReactiveFormsModule} from '@angular/forms';
+import {AppComponent} from './app.component';
+import {MyDirectiveDirective} from './my-directive.directive';
+import {MyPipePipe} from './my-pipe.pipe';
+import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {HomeComponent} from './home/home.component';
+import {LoginComponent} from './login/login.component';
+import {NewsComponent} from './news/news.component';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {RouterModule, Routes} from '@angular/router';
 
+
+const appRotes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {page: 'Home page'}
+  },
+  {
+    path: 'news/:newsTs',
+    component: NewsComponent
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  },
+];
 
 
 @NgModule({
@@ -14,14 +44,20 @@ import {HttpClientModule} from '@angular/common/http';
     AppComponent,
     MyDirectiveDirective,
     MyPipePipe,
+    HomeComponent,
+    LoginComponent,
+    NewsComponent,
+    NotFoundComponent,
 
   ],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRotes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
